0bf949a38df0ffc97b92d611cce76828ddd539f4
Enrique Turcott <enrique.turcott@cimav.edu.mx>
2012-11-28 16:52:39 -0700

    Fix image uploader

--- app/uploaders/staff_image_uploader.rb
+++ app/uploaders/staff_image_uploader.rb
@@ -4,7 +4,8 @@ class StaffImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
+  include Sprockets::Helpers::RailsHelper
+  include Sprockets::Helpers::IsolatedHelper
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
   # include Sprockets::Helpers::RailsHelper
   # include Sprockets::Helpers::IsolatedHelper
@@ -57,5 +58,4 @@ class StaffImageUploader < CarrierWave::Uploader::Base
   # def filename
   #   "something.jpg" if original_filename
   # end
-
 end
