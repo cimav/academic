9264709ab1f0cc45ad3a77c5fa6340ac6d016c82
Enrique Turcott <enrique.turcott@cimav.edu.mx>
2019-02-19 19:43:17 -0700

    Change term for enrollment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,8 +2,8 @@
 class StudentsController < ApplicationController
   before_filter :auth_required
   respond_to :html, :xml, :json
-  $CICLO = "2018-1"
-  $NCICLO = "2018-2"
+  $CICLO = "2018-2"
+  $NCICLO = "2019-1"
 
   def enrollment_courses
     @student   = Student.find(params[:id])
